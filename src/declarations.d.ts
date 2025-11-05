1fcf596818e01829775a0867f4bca58cb018e862
CD Cabrera <ccabrera@redhat.com>
2025-11-05 12:38:47 -0500

    DEV part 3

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,27 +1,7 @@
-// Create a custom declaration file for the @patternfly/patternfly-component-schemas module
-// This file tells TypeScript the basic types for the module since it doesn't provide its own.
+// Type declarations for @patternfly/patternfly-component-schemas/json
+// This file is needed because the package doesn't export TypeScript types.
+// TODO: Remove this file once the package exports its own types.
 
-declare module '@patternfly/patternfly-component-schemas' {
-  /**
-   * An array of all available PatternFly component names.
-   */
-  export const componentNames: string[];
-
-  /**
-   * A function that retrieves the JSON schema for a given component.
-   *
-   * @param componentName The name of the component to get the schema for.
-   * @return A promise that resolves with the component schema information.
-   */
-  export function getComponentSchema(componentName: string): Promise<{
-    componentName: string;
-    propsCount: number;
-    requiredProps: string[];
-    schema: Record<string, any>;
-  }>;
-}
-
-// Declare the /json subpath export
 declare module '@patternfly/patternfly-component-schemas/json' {
   /**
    * An array of all available PatternFly component names.
