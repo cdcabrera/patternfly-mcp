6ee490ee5ad05ffd869360415fffc6bb198c01eb
CD Cabrera <ccabrera@redhat.com>
2025-11-23 23:48:15 -0500

    DEV stash

--- src/docs.chart.ts
+++ src/docs.chart.ts
@@ -1,21 +1,21 @@
-import { PF_EXTERNAL_CHARTS_COMPONENTS, PF_EXTERNAL_CHARTS_DESIGN } from './options.defaults';
+import { PF_EXTERNAL_CHARTS_COMPONENTS } from './options.defaults';
 
 const CHART_DOCS = [
   `[@patternfly/Charts - Colors for Charts](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartTheme/examples/ChartTheme.md)`,
-  `[@patternfly/Charts - Target Chart](${PF_EXTERNAL_CHARTS_DESIGN}/area-chart/area-chart.md)`,
-  `[@patternfly/Charts - Bar Chart](${PF_EXTERNAL_CHARTS_DESIGN}/bar-chart/bar-chart.md)`,
-  `[@patternfly/Charts - Box Plot Chart](${PF_EXTERNAL_CHARTS_DESIGN}/ChartBoxPlot/examples/ChartBoxPlot.md)`,
-  `[@patternfly/Charts - Bullet Chart](${PF_EXTERNAL_CHARTS_DESIGN}/bullet-chart/bullet-chart.md)`,
-  `[@patternfly/Charts - Donut Chart](${PF_EXTERNAL_CHARTS_DESIGN}/donut-chart/donut-chart.md)`,
-  `[@patternfly/Charts - Donut Utilization Chart](${PF_EXTERNAL_CHARTS_DESIGN}/donut-utilization-chart/donut-utilization-chart.md)`,
-  `[@patternfly/Charts - Line Chart](${PF_EXTERNAL_CHARTS_DESIGN}/line-chart/line-chart.md)`,
-  `[@patternfly/Charts - Pie Chart](${PF_EXTERNAL_CHARTS_DESIGN}/pie-chart/pie-chart.md)`,
-  `[@patternfly/Charts - Scatter Chart](${PF_EXTERNAL_CHARTS_DESIGN}/scatter-chart/scatter-chart.md)`,
-  `[@patternfly/Charts - Sparkline Chart](${PF_EXTERNAL_CHARTS_DESIGN}/sparkline-chart/sparkline-chart.md)`,
-  `[@patternfly/Charts - Stack Chart](${PF_EXTERNAL_CHARTS_DESIGN}/stacked-chart/stacked-chart.md)`,
-  `[@patternfly/Charts - Threshold Chart](${PF_EXTERNAL_CHARTS_DESIGN}/threshold-chart/threshold-chart.md)`,
-  `[@patternfly/Charts - Legend](${PF_EXTERNAL_CHARTS_DESIGN}/legend-chart/legend-chart.md)`,
-  `[@patternfly/Charts - Tooltip](${PF_EXTERNAL_CHARTS_DESIGN}/tooltip-chart/tooltip-chart.md)`
+  `[@patternfly/Charts - Target Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartArea/examples/ChartArea.md)`,
+  `[@patternfly/Charts - Bar Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartBar/examples/ChartBar.md)`,
+  `[@patternfly/Charts - Box Plot Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartBoxPlot/examples/ChartBoxPlot.md)`,
+  `[@patternfly/Charts - Bullet Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartBullet/examples/ChartBullet.md)`,
+  `[@patternfly/Charts - Donut Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartDonut/examples/ChartDonut.md)`,
+  `[@patternfly/Charts - Donut Utilization Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartDonutUtilization/examples/ChartDonutUtilization.md)`,
+  `[@patternfly/Charts - Line Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartLine/examples/ChartLine.md)`,
+  `[@patternfly/Charts - Pie Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartPie/examples/ChartPie.md)`,
+  `[@patternfly/Charts - Scatter Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartScatter/examples/ChartScatter.md)`,
+  `[@patternfly/Charts - Sparkline Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/Sparkline/examples/sparkline.md)`,
+  `[@patternfly/Charts - Stack Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartStack/examples/ChartStack.md)`,
+  `[@patternfly/Charts - Threshold Chart](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartThreshold/examples/ChartThreshold.md)`,
+  `[@patternfly/Charts - Legend](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartLegend/examples/ChartLegend.md)`,
+  `[@patternfly/Charts - Tooltip](${PF_EXTERNAL_CHARTS_COMPONENTS}/ChartTooltip/examples/ChartTooltip.md)`
 ];
 
 export { CHART_DOCS };
