cd1e7a30daec61553675f4e79051fad32e9806ba
CD Cabrera <cabrera.code@gmail.com>
2025-11-25 21:15:27 -0500

    DEV clean up index

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 // import { fileURLToPath } from 'node:url';
 import { parseCliOptions, type CliOptions } from './options';
 import { setOptions } from './options.context';
-import { runServer, type ServerInstance } from './server';
+import { runServer, type ServerInstance, type ServerSettings } from './server';
 
 /**
  * Options for programmatic usage.
@@ -30,9 +30,7 @@ interface ProgrammaticOptions extends CliOptions {
  * Properties:
  * - `allowProcessExit` (optional): Override process exits.
  */
-interface ProgrammaticSettings {
-  allowProcessExit?: boolean;
-}
+type ProgrammaticSettings = Pick<ServerSettings, 'allowProcessExit'>;
 
 /**
  * Main function - CLI entry point with optional programmatic overrides
@@ -51,23 +49,13 @@ const main = async (
   programmaticOptions?: Partial<ProgrammaticOptions>,
   { allowProcessExit }: ProgrammaticSettings = {}
 ): Promise<ServerInstance> => {
-  // this works
-  // const updatedAllowProcessExit = allowProcessExit ?? (programmaticOptions?.mode === 'cli' || programmaticOptions?.mode === 'programmatic');
-
-  // this doesn't work
-  // const isTestMode = programmaticOptions?.mode === 'test';
-  // const updatedAllowProcessExit = allowProcessExit ?? !isTestMode;
-
-  // this doesn't work
   const updatedAllowProcessExit = allowProcessExit ?? programmaticOptions?.mode !== 'test';
 
   try {
     const cliOptions = parseCliOptions();
-
-    // Apply options to context. setOptions merges with DEFAULT_OPTIONS internally
     const mergedOptions = setOptions({ ...cliOptions, ...programmaticOptions });
-    // console.log('>>>>>>>> SERVER', updatedAllowProcessExit);
 
+    // `runServer` doesn't require it, but `memo` does for "uniqueness", pass in the merged options for a hashable argument
     return await runServer.memo(mergedOptions, { allowProcessExit: updatedAllowProcessExit });
   } catch (error) {
     console.error('Failed to start server:', error);
@@ -94,7 +82,6 @@ if (isMain) {
 try {
   const isCli = process.env.NODE_ENV !== 'local' ||
     (typeof process.argv[1] === 'string' && import.meta.url === new URL(process.argv[1], 'file:').href);
-  // const isCli = process.env.NODE_ENV !== 'local';
 
   if (isCli) {
     main({ mode: 'cli' }).catch(error => {
