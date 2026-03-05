60dcc0b02b5856aea35e2109262160b9757b4b74
CD Cabrera <cabrera.code@gmail.com>
2026-03-05 10:27:51 -0500

    fix: review update

--- src/__tests__/docs.json.test.ts
+++ src/__tests__/docs.json.test.ts
@@ -20,10 +20,22 @@ describe('docs.json', () => {
 
   it('should have unique links per each entry', () => {
     const allLinks = new Set<string>();
+    const baseLinks = new Set<string | undefined>();
     const flatDocs = Object.values(docs.docs).flat();
 
-    flatDocs.forEach(entry => allLinks.add(entry.path));
+    flatDocs.forEach(entry => {
+      allLinks.add(entry.path);
 
+      if (entry.path.includes('documentation:')) {
+        baseLinks.add('documentation:');
+      } else if (entry.path.includes('/patternfly/patternfly-org/')) {
+        baseLinks.add(entry.path.split('/patternfly/patternfly-org/')[1]?.split('/')[0]);
+      } else if (entry.path.includes('/patternfly/patternfly-react/')) {
+        baseLinks.add(entry.path.split('/patternfly/patternfly-react/')[1]?.split('/')[0]);
+      }
+    });
+
+    expect(Array.from(baseLinks)).toMatchSnapshot('limited version hashes');
     expect(allLinks.size).toBe(flatDocs.length);
     expect(allLinks.size).toBe(docs.meta.totalDocs);
   });
