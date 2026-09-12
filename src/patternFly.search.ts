e89aa97f1cdafdb4b5e06c3778be4da9e231ffff
CD Cabrera <cabrera.code@gmail.com>
2026-09-11 23:30:34 -0400

    DEV stash

--- src/patternFly.search.ts
+++ src/patternFly.search.ts
@@ -441,7 +441,7 @@ const dynamicFilterPatternFly = async (
   let isDynamicLimit = false;
   let updatedMaxResultsLimit = maxResultsLimit ?? 1;
 
-  if (maxResultsLimit === undefined && !isPatternFlyUri(searchQuery) && !isShaHexLike(searchQuery, { minLength: 40 })) {
+  if (maxResultsLimit === undefined && !isPatternFlyUri(searchQuery) && !isShaHexLike(searchQuery)) {
     isDynamicLimit = true;
     updatedMaxResultsLimit = searchFilters.length;
   }
